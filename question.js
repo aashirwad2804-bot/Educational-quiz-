0a12622f0ad41515955288a427150cb74056225d
Black don <aashirwad2804@gmail.com>
2026-08-22 19:43:54 +0530

    Create question bank for NEET biology
    Added a question bank for NEET biology with multiple questions and options.

---
+++ question.js
@@ -0,0 +1,70 @@
+const questionBank = {
+
+    neet: {
+
+        biology: {
+
+            "Cell: The Unit of Life": [
+
+                {
+                    question: "The basic structural and functional unit of life is:",
+                    options: [
+                        "Tissue",
+                        "Cell",
+                        "Organ",
+                        "Organ system"
+                    ],
+                    answer: 1
+                },
+
+                {
+                    question: "Who coined the term 'cell'?",
+                    options: [
+                        "Robert Hooke",
+                        "Robert Brown",
+                        "Schleiden",
+                        "Schwann"
+                    ],
+                    answer: 0
+                },
+
+                {
+                    question: "Which organelle is called the powerhouse of the cell?",
+                    options: [
+                        "Golgi apparatus",
+                        "Mitochondrion",
+                        "Ribosome",
+                        "Lysosome"
+                    ],
+                    answer: 1
+                },
+
+                {
+                    question: "The plasma membrane is mainly composed of:",
+                    options: [
+                        "Lipids and proteins",
+                        "DNA and RNA",
+                        "Cellulose and starch",
+                        "Only proteins"
+                    ],
+                    answer: 0
+                },
+
+                {
+                    question: "Which organelle contains digestive enzymes?",
+                    options: [
+                        "Lysosome",
+                        "Ribosome",
+                        "Nucleus",
+                        "Centrosome"
+                    ],
+                    answer: 0
+                }
+
+            ]
+
+        }
+
+    }
+
+};
